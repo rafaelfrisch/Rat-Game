a440866193e42e8060a1ed8d0a4887d9d755b3f2
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-08-26 16:04:05 -0300

    Finish Rat movement

--- rat.js
+++ rat.js
@@ -1,14 +1,10 @@
 const screen = document.getElementById('screen')
 const rat = document.getElementById('rat')
-const cheese = document.getElementById('cheese')
 
-function initialPosition(screen, rat, cheese){
+function initialPosition(screen, rat){
     const screen_position = getCenterPosition(screen)
-    const rat_dimension = getElementDimensions(rat)
-    changePositionElement(rat, screen_position.x_position-rat_dimension.width/2,
-        screen_position.y_position-rat_dimension.height/2)
-    changePositionElement(cheese, screen_position.x_position+rat_dimension.width,
-        screen_position.y_position-rat_dimension.height/2)
+    changePositionElement(rat, screen_position.x_position,
+        screen_position.y_position)
 }
 
 function getElementDimensions(element){
@@ -27,36 +23,25 @@ function getCenterPosition(element){
     }
 }
 
-function rotateElement(element, angle){
-    element.style.transform = 'rotate('+angle*180/Math.PI+'deg)';
-}
-
 function changePositionElement(element, x_position, y_position){
     element.style.left = x_position.toString() + "px"
     element.style.top = y_position.toString() + "px"
+    element.style.transform = 'translate(-50%, -50%)'
 }
 
-// function translateElementWithMouse(element, mouse_x_position, mouse_y_position, angle, screen){
-//     const radius = Math.hypot(mouse_x_position - getCenterPosition(screen).x_position,
-//     mouse_y_position - getCenterPosition(screen).y_position
-//     )-getElementDimensions(element).width
-//     console.log(radius)
-//     element.style.left = (radius*Math.sin(angle)+getElementDimensions(element).height/2*Math.cos(angle)+
-//     getCenterPosition(screen).x_position
-//     ).toString()+ "px"
-//     element.style.top = (radius*Math.cos(angle)-getElementDimensions(element).width/2*Math.sin(angle)+
-//     getCenterPosition(screen).y_position
-//     ).toString()+ "px"
-// }
+function moveRat(element, mouse_x_position, mouse_y_position, angle){
+    element.style.left = mouse_x_position.toString() + "px"
+    element.style.top = mouse_y_position.toString() + "px"
+    element.style.transform = 'translate(-50%, -50%)'+'rotate('+angle*180/Math.PI+'deg)'
+}
 
-initialPosition(screen, rat, cheese)
+initialPosition(screen, rat)
 
 screen.addEventListener('mousemove', (event) =>{
     const mouse_x_position = event.pageX
     const mouse_y_position = event.pageY
-    changePositionElement(cheese, mouse_x_position, mouse_y_position)
     const rotation_angle = 
     Math.atan2(mouse_y_position - getCenterPosition(screen).y_position,
     mouse_x_position - getCenterPosition(screen).x_position)
-    rotateElement(rat, rotation_angle)
+    moveRat(rat, mouse_x_position, mouse_y_position, rotation_angle)
 })
